3f4b26b713340d0f33835e0fad095c85ec27fcbc
Marzi Ghasedian <marzighased@gmail.com>
2025-05-13 20:57:30 +0200

    header added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  darkMode = signal<boolean>(false);
+  
+  toggleTheme() {
+    this.darkMode.update(current => !current);
+    document.body.classList.toggle('dark-theme', this.darkMode());
+  }
 }
