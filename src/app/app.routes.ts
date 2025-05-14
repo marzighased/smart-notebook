4da0d15e8383659eba6188682fb6d6485d7afb7c
Marzi Ghasedian <marzighased@gmail.com>
2025-05-14 22:35:36 +0200

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,13 @@
 import { Routes } from '@angular/router';
+import { NoteListComponent } from './components/note-list/note-list.component';
+import { NoteFormComponent } from './components/note-form/note-form.component';
+import { NoteDetailComponent } from './components/note-detail/note-detail.component';
 
-
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'notes', pathMatch: 'full' },
+  { path: 'notes', component: NoteListComponent },
+  { path: 'notes/new', component: NoteFormComponent },
+  { path: 'notes/:id', component: NoteDetailComponent },
+  { path: 'notes/:id/edit', component: NoteFormComponent },
+  { path: '**', redirectTo: 'notes' } 
+];
