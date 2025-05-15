c3221e6629a7e9fe39c4a2d843d3470d286e1905
Marzi Ghasedian <marzighased@gmail.com>
2025-05-15 20:39:49 +0200

    Create NoteList component

--- src/app/components/note-list/note-list.component.ts
+++ src/app/components/note-list/note-list.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NotesService } from '../../services/notes.service';
 
 @Component({
   selector: 'app-note-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './note-list.component.html',
   styleUrl: './note-list.component.scss'
 })
 export class NoteListComponent {
-
+  private notesService = inject(NotesService);
+  notes = this.notesService.getNotes();
 }
